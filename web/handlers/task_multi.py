bc4bdfe83b4e100cc8b4cd411cb2e0a312d939bb
asdaragon <asd159874123@gmail.com>
2021-06-21 13:14:15 +0800

    修复任务删除后多任务处理出现500的bug 2

--- web/handlers/task_multi.py
+++ web/handlers/task_multi.py
@@ -58,56 +58,57 @@ class TaskMultiOperateHandler(BaseHandler):
             for taskid, selected  in env['selectedtasks'].items():
                 if (selected):
                     task = self.db.task.get(taskid, fields=('id',  'note', 'tplid', 'userid'))
-                    if (task['userid']) == int(userid):
-                        if (tasktype == 'disable'):
-                            self.db.task.mod(taskid, disabled = True)
-                        if (tasktype == 'enable'):
-                            self.db.task.mod(taskid, disabled = False)
-                        if (tasktype == 'delete'):
-                            logs = self.db.tasklog.list(taskid = taskid, fields=('id'))
-                            for log in logs:
-                                self.db.tasklog.delete(log['id'])
-                            self.db.task.delete(taskid)
-                        if (tasktype == 'setgroup'):
-                            group_env = env['setgroup']
-                            New_group = group_env['newgroup'].strip()
-                            if New_group != "" :
-                                target_group = New_group.decode("utf-8").encode("utf-8")
-                            else:
-                                target_group = group_env['checkgroupname'] or 'None'
+                    if (task):
+                        if (task['userid']) == int(userid):
+                            if (tasktype == 'disable'):
+                                self.db.task.mod(taskid, disabled = True)
+                            if (tasktype == 'enable'):
+                                self.db.task.mod(taskid, disabled = False)
+                            if (tasktype == 'delete'):
+                                logs = self.db.tasklog.list(taskid = taskid, fields=('id'))
+                                for log in logs:
+                                    self.db.tasklog.delete(log['id'])
+                                self.db.task.delete(taskid)
+                            if (tasktype == 'setgroup'):
+                                group_env = env['setgroup']
+                                New_group = group_env['newgroup'].strip()
+                                if New_group != "" :
+                                    target_group = New_group.decode("utf-8").encode("utf-8")
+                                else:
+                                    target_group = group_env['checkgroupname'] or 'None'
 
-                            self.db.task.mod(taskid, groups=target_group)
+                                self.db.task.mod(taskid, groups=target_group)
 
-                        if (tasktype == 'settime'):
-                            time_env = env['settime']
-                            c = cal()
-                            settime_env = {
-                                'sw': True,
-                                'time': time_env['ontime_val'],
-                                'mode': time_env['ontime_method'],
-                                'date': time_env['ontime_run_date'],
-                                'tz1': time_env['randtimezone1'],
-                                'tz2': time_env['randtimezone2'],
-                                'cron_val': time_env['cron_val'],
-                            }
+                            if (tasktype == 'settime'):
+                                time_env = env['settime']
+                                c = cal()
+                                settime_env = {
+                                    'sw': True,
+                                    'time': time_env['ontime_val'],
+                                    'mode': time_env['ontime_method'],
+                                    'date': time_env['ontime_run_date'],
+                                    'tz1': time_env['randtimezone1'],
+                                    'tz2': time_env['randtimezone2'],
+                                    'cron_val': time_env['cron_val'],
+                                }
 
-                            if (time_env['randtimezone1'] != '') and (time_env['randtimezone1'] != ''):
-                                settime_env['randsw'] = True 
-                            if (time_env['cron_sec'] != ''):
-                                settime_env['cron_sec'] = time_env['cron_sec'] 
+                                if (time_env['randtimezone1'] != '') and (time_env['randtimezone1'] != ''):
+                                    settime_env['randsw'] = True 
+                                if (time_env['cron_sec'] != ''):
+                                    settime_env['cron_sec'] = time_env['cron_sec'] 
 
-                            if (len(settime_env['time'].split(':')) == 2):
-                                settime_env['time'] = settime_env['time'] + ':00'
+                                if (len(settime_env['time'].split(':')) == 2):
+                                    settime_env['time'] = settime_env['time'] + ':00'
 
-                            tmp = c.calNextTs(settime_env)
-                            if (tmp['r'] == 'True'):
-                                self.db.task.mod(taskid, disabled = False, 
-                                                         newontime = json.dumps(settime_env), 
-                                                         next = tmp['ts'])
-                            else:
-                                raise Exception(u'参数错误')
-                    else:
-                        raise Exception('用户id与任务的用户id不一致')
+                                tmp = c.calNextTs(settime_env)
+                                if (tmp['r'] == 'True'):
+                                    self.db.task.mod(taskid, disabled = False, 
+                                                            newontime = json.dumps(settime_env), 
+                                                            next = tmp['ts'])
+                                else:
+                                    raise Exception(u'参数错误')
+                        else:
+                            raise Exception('用户id与任务的用户id不一致')
         except Exception:
             traceback.print_exc()
             self.render('utils_run_result.html', log=traceback.format_exc(), title=u'设置失败', flg='danger')
