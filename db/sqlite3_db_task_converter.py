9585501aff5bbf890debb4e3b4c92924e252a5f1
a76yyyy <q981331502@163.com>
2021-10-09 14:17:45 +0800

    修复数据库aip字段类型

--- db/sqlite3_db_task_converter.py
+++ db/sqlite3_db_task_converter.py
@@ -377,6 +377,43 @@ class DBconverter(_TaskDB, BaseDB):
             exec_shell("ALTER TABLE `user` ADD  `password_md5` VARBINARY(128) NOT NULL DEFAULT '' ") 
 
         try:
+            if self.db.user.get("1", fields=('cip','mip','aip')) and str.isdigit(str(list(self.db.user.list( fields='cip'))[0]['cip'])):
+                exec_shell("ALTER TABLE `user` RENAME TO `userold`")
+                if config.db_type == 'sqlite3':
+                    autokey = 'AUTOINCREMENT'
+                else:
+                    autokey = 'AUTO_INCREMENT'
+                exec_shell('''CREATE TABLE IF NOT EXISTS `user` (
+                    `id` INTEGER NOT NULL PRIMARY KEY  %s,
+                    `email` VARCHAR(256) NOT NULL,
+                    `email_verified` TINYINT(1) NOT NULL DEFAULT 0,
+                    `password` VARBINARY(128) NOT NULL,
+                    `password_md5` VARBINARY(128) NOT NULL DEFAULT '',
+                    `userkey` VARBINARY(128) NOT NULL,
+                    `nickname` VARCHAR(64) NULL,
+                    `role` VARCHAR(128) NULL,
+                    `ctime` INT UNSIGNED NOT NULL,
+                    `mtime` INT UNSIGNED NOT NULL,
+                    `atime` INT UNSIGNED NOT NULL,
+                    `cip` VARBINARY(16) NOT NULL,
+                    `mip` VARBINARY(16) NOT NULL,
+                    `aip` VARBINARY(16) NOT NULL,
+                    `skey` VARBINARY(128) NOT NULL DEFAULT '',
+                    `barkurl` VARBINARY(128) NOT NULL DEFAULT '',
+                    `wxpusher` VARBINARY(128) NOT NULL DEFAULT '',
+                    `noticeflg` INT UNSIGNED NOT NULL DEFAULT 1,
+                    `logtime`  VARBINARY(1024) NOT NULL DEFAULT '{"en":false,"time":"20:00:00","ts":0,"schanEn":false,"WXPEn":false}',
+                    `status`  VARBINARY(1024) NOT NULL DEFAULT 'Enable',
+                    `notepad` TEXT NULL,
+                    `diypusher` VARBINARY(1024) NOT NULL DEFAULT '',
+                    `qywx_token` VARBINARY(1024) NOT NULL DEFAULT ''
+                    );''' % autokey)
+                exec_shell("INSERT INTO `user` SELECT `id`,`email`,`email_verified`,`password`,`password_md5`,`userkey`,`nickname`,`role`,`ctime`,`mtime`,`atime`,`cip`,`mip`,`aip`,`skey`,`barkurl`,`wxpusher`,`noticeflg`,`logtime`,`status`,`notepad`,`diypusher`,`qywx_token` FROM `userold` ")
+                exec_shell("DROP TABLE `userold` ")
+        except :
+            pass
+
+        try:
             self.db.site.get("1", fields=('repos'))
         except :
             if config.db_type == 'sqlite3':
