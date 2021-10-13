6eeaef52182b023f02d2be6fb4a531e8cd306d23
a76yyyy <q981331502@163.com>
2021-10-13 17:45:46 +0800

    添加更新提示

--- update.sh
+++ update.sh
@@ -3,3 +3,6 @@ cd /usr/src/app
 git fetch --all
 git reset --hard origin/master
 git pull
+if [ $AUTO_RELOAD ] && [ "$AUTO_RELOAD" == "False" ];then
+    echo "Info: 请手动重启容器，或设置环境变量AUTO_RELOAD以开启热更新功能"
+fi
