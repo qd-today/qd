9c27c2820a9f5151de1093389fad2f8fe4aa35b6
a76yyyy <q981331502@163.com>
2025-01-29 11:51:16 +0800

    style(qd): ✨ 优化端口检查超时设置

--- qd.py
+++ qd.py
@@ -17,20 +17,21 @@ from run import start_server
 
 config.display_import_warning = False
 
-logger_qd = Log('QD').getlogger()
+logger_qd = Log("QD").getlogger()
 
 # 判断 端口 是否被占用
 
 
 def check_port(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5)
     try:
-        s.connect(('127.0.0.1', port))
+        s.connect(("127.0.0.1", port))
         s.shutdown(2)
-        logger_qd.debug('Port %s is used' , port)
+        logger_qd.debug("Port %s is used", port)
         return False
     except Exception:
-        logger_qd.debug('Port %s is available' , port)
+        logger_qd.debug("Port %s is available", port)
         return True
 
 
@@ -39,7 +40,7 @@ def usage():
     sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) < 3:
         usage()
 
@@ -47,7 +48,7 @@ if __name__ == '__main__':
     tpl_file = sys.argv[1]
     try:
         # deepcode ignore PT: tpl_file is a file
-        tpl = json.load(open(tpl_file, encoding='utf-8'))
+        tpl = json.load(open(tpl_file, encoding="utf-8"))
     except Exception as e:
         logger_qd.error(e, exc_info=config.traceback_print)
         usage()
@@ -57,33 +58,37 @@ if __name__ == '__main__':
     env = {}
     ENV_FILE = None
     for each in sys.argv[2:]:
-        if each.startswith('--'):
-            key, value = each.split('=', 1)
-            key = key.lstrip('--')
+        if each.startswith("--"):
+            key, value = each.split("=", 1)
+            key = key.lstrip("--")
             variables[key] = value
         else:
             ENV_FILE = each
     if ENV_FILE:
         try:
             # deepcode ignore PT: env_file is a file
-            env = json.load(open(ENV_FILE, encoding='utf-8'))
+            env = json.load(open(ENV_FILE, encoding="utf-8"))
         except Exception as e:
             logger_qd.error(e, exc_info=config.traceback_print)
             usage()
-    if 'variables' not in env or not isinstance(env['variables'], dict) \
-            or 'session' not in env:
+    if (
+        "variables" not in env
+        or not isinstance(env["variables"], dict)
+        or "session" not in env
+    ):
         env = {
-            'variables': env,
-            'session': [],
+            "variables": env,
+            "session": [],
         }
-    env['variables'].update(variables)
+    env["variables"].update(variables)
 
     MANUAL_START = check_port(config.port)
     if MANUAL_START:
-        logger_qd.info('QD service is not running on port %s' , config.port)
-        logger_qd.info('QD service will be started on port %s' , config.port)
+        logger_qd.info("QD service is not running on port %s", config.port)
+        logger_qd.info("QD service will be started on port %s", config.port)
         # 创建新进程, 以执行 run 中的 main 异步函数
         import multiprocessing
+
         p = multiprocessing.Process(target=start_server)
         p.start()
         # 循环检测端口是否被占用, 如果被占用, 则继续下一步
@@ -92,26 +97,32 @@ if __name__ == '__main__':
                 break
             else:
                 import time
+
                 time.sleep(1)
     else:
-        logger_qd.info('QD service is running on port %s' , config.port)
+        logger_qd.info("QD service is running on port %s", config.port)
 
     # do fetch
     ioloop = asyncio.new_event_loop()
     asyncio.set_event_loop(ioloop)
-    result: asyncio.Task = asyncio.ensure_future(Fetcher().do_fetch(tpl, env), loop=ioloop)
-    logger_qd.info('QD start to do fetch: %s', tpl_file)
+    result: asyncio.Task = asyncio.ensure_future(
+        Fetcher().do_fetch(tpl, env), loop=ioloop
+    )
+    logger_qd.info("QD start to do fetch: %s", tpl_file)
     ioloop.run_until_complete(result)
     ioloop.stop()
 
     try:
         env, _ = result.result()
     except Exception as e:
-        logger_qd.error('QD failed: %s', e, exc_info=config.traceback_print)
+        logger_qd.error("QD failed: %s", e, exc_info=config.traceback_print)
     else:
-        logger_qd.info('QD success! Results:\n %s', env.get('variables', {}).get('__log__', '').replace('\\r\\n', '\r\n'))
+        logger_qd.info(
+            "QD success! Results:\n %s",
+            env.get("variables", {}).get("__log__", "").replace("\\r\\n", "\r\n"),
+        )
 
     if MANUAL_START:
         p.terminate()
         p.join()
-        logger_qd.info('QD service is ended. ')
+        logger_qd.info("QD service is ended. ")
